928b88c59ea3c61753a79c28bbc8d4088b4c6c61
step6829 <stephen.lowrie@rackspace.com>
2015-09-10 13:12:14 -0500

    update

--- environment_setup.sh
+++ environment_setup.sh
@@ -6,7 +6,7 @@ if [ ! -e "/usr/bin/docker" ]; then
     wget -qO- https://get.docker.com/ | sh
 fi
 
-if [ ! -e "/usr/local/bin/docker-compose"]; then
+if [ ! -e "/usr/local/bin/docker-compose" ]; then
     curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 fi
