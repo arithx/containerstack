09f39fd40e9a21e007a6710cd3c7ce9898edce0a
step6829 <stephen.lowrie@rackspace.com>
2015-09-10 13:03:13 -0500

    Updated wget in Dockerfile & add environment_setup

---
+++ environment_setup.sh
@@ -0,0 +1,12 @@
+if [ ! -e "/usr/bin/git" ] then
+    apt-get install git
+fi
+
+if [ ! -e "/usr/bin/docker" ] then
+    wget -qO- https://get.docker.com/ | sh
+fi
+
+if [ ! -e "/usr/local/bin/docker-compose"] then
+    curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+fi
