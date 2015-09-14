466014da5322ce220220741f356a87a2cefbc5f4
step6829 <stephen.lowrie@rackspace.com>
2015-09-14 16:24:04 -0500

    User handling

--- nodocker.sh
+++ nodocker.sh
@@ -1,13 +1,16 @@
-if [ ! -e "/usr/bin/git" ]; then
-    apt-get install -qqy git
+if [ "$(whoami)" != "stack" ] then
+  if [ ! -e "/usr/bin/git" ]; then
+      apt-get install -qqy git
+  fi
+
+  apt-get update
+  apt-get -qqy install git socat curl sudo vim wget net-tools git socat curl sudo vim wget net-tools libffi-dev libkrb5-dev libev-dev libvirt-dev libsqlite3-dev libxml2-dev libxslt-dev libpq-dev libssl-dev libyaml-dev make build-essential python-dev libffi-dev libssl-dev
+
+  git clone https://github.com/openstack-dev/devstack
+  devstack/tools/create-stack-user.sh
+  chown -R stack devstack
+  cd devstack
+  wget https://raw.github.com/arithx/containerstack/master/localrc
+else
+  exec sudo -u stack ./stack.sh
 fi
-
-apt-get update
-apt-get -qqy install git socat curl sudo vim wget net-tools git socat curl sudo vim wget net-tools libffi-dev libkrb5-dev libev-dev libvirt-dev libsqlite3-dev libxml2-dev libxslt-dev libpq-dev libssl-dev libyaml-dev make build-essential python-dev libffi-dev libssl-dev
-
-git clone https://github.com/openstack-dev/devstack
-devstack/tools/create-stack-user.sh
-chown -R stack devstack
-cd devstack
-wget https://raw.github.com/arithx/containerstack/master/localrc
-exec sudo -u stack ./stack.sh
