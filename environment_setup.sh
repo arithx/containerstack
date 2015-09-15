897f6802952fb433327e9ec61d671bb5c7126db5
step6829 <stephen.lowrie@rackspace.com>
2015-09-15 13:11:29 -0500

    Update for CoreOS

--- environment_setup.sh
+++ environment_setup.sh
@@ -1,14 +1,27 @@
-if [ ! -e "/usr/bin/git" ]; then
-    apt-get install -qqy git
-fi
+if [ cat /etc/*-release | egrep "DISTRIB_ID=(.*)" | sed "s/DISTRIB_ID=//g" = "CoreOS" ]; then
+  if [ ! -e "/opt" ]; then
+    mkdir /opt
+  fi
+  if [ ! -e "/opt/bin" ]; then
+    mkdir /opt/bin
+  fi
+  if [ ! -e "/opt/bin/docker-compose" ]; then
+    curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /opt/bin/docker-compose
+    chmod +x /opt/bin/docker-compose
+  fi
+else
+  if [ ! -e "/usr/bin/git" ]; then
+      apt-get install -qqy git
+  fi
 
-if [ ! -e "/usr/bin/docker" ]; then
-    wget -qO- https://get.docker.com/ | sh
-fi
+  if [ ! -e "/usr/bin/docker" ]; then
+      wget -qO- https://get.docker.com/ | sh
+  fi
 
-if [ ! -e "/usr/local/bin/docker-compose" ]; then
-    curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
+  if [ ! -e "/usr/local/bin/docker-compose" ]; then
+      curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+      chmod +x /usr/local/bin/docker-compose
+  fi
 fi
 
 git clone https://github.com/arithx/containerstack
