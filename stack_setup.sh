273be0a4f8ecbfc704672b091d1d925bcae93f1d
step6829 <stephen.lowrie@rackspace.com>
2015-09-15 10:45:00 -0500

    update

--- stack_setup.sh
+++ stack_setup.sh
@@ -1,4 +1,6 @@
-cd ~/devstack
+cd ~
+git clone https://github.com/openstack-dev/devstack
+cd devstack
 wget https://raw.github.com/arithx/containerstack/master/localrc
 chmod +755 stack.sh
 ./stack.sh
